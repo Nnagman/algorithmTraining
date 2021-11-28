83af61f8789299aa1669ef37e613a867af6a88d9
nnagman <ckdgh930314@naver.com>
2021-11-28 23:16:40 +0900

    여행경로 DONE

--- Programmers/src/practice/stackAndQueue/functionDevelopment/FunctionDevelopment.java
+++ Programmers/src/practice/stackAndQueue/functionDevelopment/FunctionDevelopment.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public class FunctionDevelopment {
     public static void main(String[] args) {
-        int[] progresses = {20, 99, 93, 30, 55, 10};
-        int[] speeds = {5, 10, 1, 1, 30, 5};
+        int[] progresses = {96, 94};
+        int[] speeds = {3, 3};
         System.out.println(Arrays.toString(solution(progresses, speeds)));
     }
 
@@ -16,33 +16,21 @@ public class FunctionDevelopment {
         List<Integer> progressList = new ArrayList<>();
 
         for (int i = 0; i < progresses.length; i++) {
-            progressList.add((int) Math.ceil((100 - progresses[i]) / speeds[i]));
+            progressList.add((int) Math.ceil((float) (100 - progresses[i]) / speeds[i]));
         }
 
         System.out.println(progressList);
 
         while (progressList.size() != 0) {
-            List<Integer> idxList = new ArrayList<>();
-            int number = 0;
             int cnt = 0;
-
-            for (Integer integer : progressList)
-                if (number > integer)
-                    number = integer;
-
-
-            for (int i = 0; i < progressList.size(); i++)
-                if (number == progressList.get(i)) {
-                    cnt = cnt + 1;
-                    idxList.add(i);
-                }
-
-            for (int idx : idxList) {
-                progressList.remove(idx);
+            int number = progressList.get(0);
+            for (Integer progress : progressList) {
+                if (number >= progress) cnt += 1;
+                else break;
             }
-
             answer.add(cnt);
-            break;
+            progressList = progressList.subList(cnt, progressList.size());
+            System.out.println(number + " " + progressList + " " + answer);
         }
 
         return answer.stream().mapToInt(i -> i).toArray();
